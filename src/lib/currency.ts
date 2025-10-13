9c97f63cac5fb8bcdc643fcc56a0de799664ddd9
GRGY <80850348+grgyone@users.noreply.github.com>
2025-10-13 10:47:00 +0300

    Delete src directory

--- src/lib/currency.ts
+++
@@ -1,4 +0,0 @@
-export function formatCurrency(value: number, currency = 'RUB') {
-  const symbol = currency === 'RUB' ? '\u20bd' : currency;
-  return `${value.toLocaleString('ru-RU')} ${symbol}`;
-}
